f1231718ffe06dc08f6a23874c4b47a79a795691
Sebastian Crossa <crossasebastian@gmail.com>
2019-06-30 11:09:19 -0600

    Imported css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  return <div className='App'>Github discovery</div>;
+class App extends Component {
+  render() {
+    return <div className='App'>Github discovery</div>;
+  }
 }
 
 export default App;
